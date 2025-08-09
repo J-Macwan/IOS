1f63df5c65313b2f195cd3ea5a90744f56c02a70
Joy macwan <joymacwan@zohomail.in>
2025-08-09 12:20:12 +0530

    Add files via upload

---
+++ Project_to_do_2/Project_2/Project_2/Project_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Project_2App.swift
+//  Project_2
+//
+//  Created by JOY MACWAN on 09/08/25.
+//
+
+import SwiftUI
+
+@main
+struct Project_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
